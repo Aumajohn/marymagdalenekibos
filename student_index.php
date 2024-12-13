547f11ce5f242436973b94e4adb3ceae027a505d
Aumajohn <aumajohn443@gmail.com>
2024-12-13 22:54:04 +0300

    Add files via upload

---
+++ student_index.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>golden goro cbo</title>
+
+	<style type="text/css">
+	body{
+		
+		background-image-position:center; 
+		background-size: cover;
+		font-family: arial;
+	}
+		
+ul{
+	margin: 0px;
+	padding: 0px;
+list-style:none;
+}
+ul li{
+	float: left;
+	width: 250px;
+	height: 40px;
+	background-color: black;
+	opacity: .7;
+	line-height: 40px;
+	text-align: center;
+	list-style:none;
+	font-size: 20px;
+	margin-right: 3px;
+}
+ul li a{
+	color: white;
+	display: block;
+
+	text-decoration: none;
+}
+ul li a:hover{
+
+	background-color: darkblue;
+}
+ul li ul li{display: none;}
+ul li:hover ul li{
+	display: block;
+}
+a img li a{
+	display: none;
+	text-decoration: none;
+}
+	</style>
+
+<!-- <div><center> <a href="student_index.php" style="text-decoration: none;color: blue"><img src="com.jpg" width="140" height="150"></a></center><div><br> -->
+
+</head>
+
+
+<!-- <a href="book.php" style="float:; text-decoration: none; color: blue;"> <head><center></center></a><br>
+ -->
+<body style="background-color: darkgrey;">
+
+</a>
+
+<center>
+<ul>
+
+	<li><a href="expire.php">EXAMS AT A GLANCE</a></li>
+	<li><a  >LAUCH MESSAGE</a>
+	<ul>
+		<li><a href="student_staff_login.php" >STUDENT</a></li>
+		<li><a href="stafflogin.php">EMPLOYEE</a></li>
+	
+		
+		</ul>
+	</li>
+	<li><a href="guide.php" >HELP</a>
+		
+	</li>
+	<li><a href="login2.php">LOG OUT</a></li>
+</ul>
+
+</div></center><br><br>
+<hr color="black" width="100%"></b><br>
+</body>
+<center><footer valign="bottom" style="background-color: ; bottom-margin 0;"> &copy Complaint And Suggestion Application System.All rights Reserved.
+     <?php echo date ('D') ?> <?php echo date ('d') ?> <?php echo date ('M') ?> <?php echo date('Y') ?> </footer></center> 
+</html>
